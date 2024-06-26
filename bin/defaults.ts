54080756a60628384ad9268690d372796a01d70a
细雨独舟 <zhous@vip.qq.com>
2024-06-26 14:04:42 +0800

    Update defaults.ts
    Signed-off-by: 细雨独舟 <zhous@vip.qq.com>

--- bin/defaults.ts
+++ bin/defaults.ts
@@ -24,8 +24,8 @@ export const DEFAULT_PAKE_OPTIONS: PakeCliOptions = {
 // Just for cli development
 export const DEFAULT_DEV_PAKE_OPTIONS: PakeCliOptions & { url: string } = {
   ...DEFAULT_PAKE_OPTIONS,
-  url: 'https://weread.qq.com',
-  name: 'WeRead',
-  safeDomain: ['weread.qq.com'],
+  url: 'https://pms.lyetc.com',
+  name: '智停云岗亭端',
+  safeDomain: ['pms.lyetc.com'],
   hideTitleBar: true,
 };
